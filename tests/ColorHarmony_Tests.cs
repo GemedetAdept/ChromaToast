b25f9cb1f77e0ae94f10cf874d247be2ab4b96fe
Vivian Zitek <vivian.zitek@gmail.com>
2023-05-21 20:15:51 -0400

    Create tests file for ColorHarmony.

---
+++ tests/ColorHarmony_Tests.cs
@@ -0,0 +1,11 @@
+using colorvalues;
+using colorharmony;
+namespace tests {
+public class ColorHarmony_Tests {
+
+	public int Complementary_Returns_Valid_Complementary() {
+
+		var inputHSV = new Color.HSV(174.0, 85.0, 56.0);
+	}
+}
+}
