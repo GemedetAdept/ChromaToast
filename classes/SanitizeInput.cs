01fd33731bfe43960e532806644648dc766ec195
Vivian Zitek <vivian.zitek@gmail.com>
2023-05-29 20:22:15 -0400

    Create StripCharacter method in SanitizeInput.

--- classes/SanitizeInput.cs
+++ classes/SanitizeInput.cs
@@ -1,5 +1,13 @@
 namespace userinput {
 public class SanitizeInput {
 
+	public static string StripCharacter(string inputString, char targetCharacter) {
+
+		if (inputString.Contains(targetCharacter) {
+			inputString = inputString.Replace(targetCharacter, "");
+		}
+
+		return inputString;
+	}
 }
 }
