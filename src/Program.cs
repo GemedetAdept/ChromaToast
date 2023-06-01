2c767c2cb76b0c86f6b75f789b020a06b0a80154
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-01 19:51:45 -0400

    Implement Run_ColorHarmony_Tests.

--- src/Program.cs
+++ src/Program.cs
@@ -388,27 +388,7 @@ void DebugBox() {
 	TestsDriver.Initialize();
 	TestsDriver.Run_Color_Tests(false);
 
-	// Console.Clear();
-	// Console.WriteLine("-- Color_Tests --");
-
-	// Color_Tests.CMYK_Returns_Type_When_Called();
-	// Color_Tests.HEX_Returns_Type_When_Called();
-	// Color_Tests.HSL_Returns_Type_When_Called();
-	// Color_Tests.HSV_Returns_Type_When_Called();
-	// Color_Tests.RGB_Returns_Type_When_Called();
-
-	Console.WriteLine("");
-	Console.WriteLine("-- ColorHarmony_Tests --");
-
-	ColorHarmony_Tests.Complementary_Hue_Difference_Is_180();
-	ColorHarmony_Tests.SplitComplementary_Hue_Difference_Is_210_150();
-	ColorHarmony_Tests.Triadic_Hue_Difference_Is_120();
-	ColorHarmony_Tests.Tetradic_Hue_Difference_Is_240_180_240();
-	ColorHarmony_Tests.Square_Hue_Difference_Is_90();
-	ColorHarmony_Tests.Analogous_Hue_Difference_Is_30();
-	ColorHarmony_Tests.capValue_Above_Ceiling_Subtracts_Ceiling();
-	ColorHarmony_Tests.capValue_Below_Floor_Adds_Ceiling();
-	ColorHarmony_Tests.capValue_Handles_Input_Below_Negative_Ceiling();
+	TestsDriver.Run_ColorHarmony_Tests(true);
 
 	Console.WriteLine("");
 	Console.WriteLine("-- ConvertColor_Tests --");
--- tests/TestsDriver.cs
+++ tests/TestsDriver.cs
@@ -18,9 +18,29 @@ public class TestsDriver {
 		Color_Tests.HSL_Returns_Type_When_Called();
 		Color_Tests.HSV_Returns_Type_When_Called();
 		Color_Tests.RGB_Returns_Type_When_Called();
+
+		Console.WriteLine("");
 		return 1;
+	} return 0;
 	}
-	return 1;
+
+	public static int Run_ColorHarmony_Tests(bool toggle) {
+	if (toggle == true) {
+		Console.WriteLine("-- ColorHarmony_Tests --");
+
+		ColorHarmony_Tests.Complementary_Hue_Difference_Is_180();
+		ColorHarmony_Tests.SplitComplementary_Hue_Difference_Is_210_150();
+		ColorHarmony_Tests.Triadic_Hue_Difference_Is_120();
+		ColorHarmony_Tests.Tetradic_Hue_Difference_Is_240_180_240();
+		ColorHarmony_Tests.Square_Hue_Difference_Is_90();
+		ColorHarmony_Tests.Analogous_Hue_Difference_Is_30();
+		ColorHarmony_Tests.capValue_Above_Ceiling_Subtracts_Ceiling();
+		ColorHarmony_Tests.capValue_Below_Floor_Adds_Ceiling();
+		ColorHarmony_Tests.capValue_Handles_Input_Below_Negative_Ceiling();
+
+		Console.WriteLine("");
+		return 1;
+	} return 0;
 	}
 }
 }
