28315bbff73fe10b697ee9916624fc8be91c716c
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-02 17:16:57 -0400

    Add property CursorSpacer. Fill out MenuBeta default values.

--- src/Menus/MenuBeta.cs
+++ src/Menus/MenuBeta.cs
@@ -4,6 +4,7 @@ public class MenuBeta {
 	private string _title;
 	private string[] _options;
 	private string _cursor;
+	private int _cursorSpacer;
 	private int _activeItem;
 	private int _selectedItem;
 	private bool _menuLoop;
@@ -11,12 +12,27 @@ public class MenuBeta {
 	public string Title {get;set;}
 	public string[] Options{get;set;}
 	public string Cursor {get;set;}
+	public string CursorSpacer {get;set;}
 	public int ActiveItem {get;set;}
 	public int SelectedItem{get;set;}
 	public bool MenuLoop;
 
+	public MenuBeta() {
+		Title = "-- Lorem Ipsum --";
+		Options = new string[] {""};
+		Cursor = ">";
+		CursorSpacer = " ";
+		ActiveItem = 0;
+		SelectedItem = -1; // Initalize with non-functional value
+		MenuLoop = true;
+	}
+
+	public int LoadMenu() {
+		return 1;
+	}
+
 	public int LoadCursor() {
-		
+		return 1;
 	}
 }
 }
