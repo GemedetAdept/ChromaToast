de0e527750696fb818da9ab6dc464cac2c49b008
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-02 02:28:39 -0400

    Initialize objects for color harmony types.

--- src/classes/GenerationDriver.cs
+++ src/classes/GenerationDriver.cs
@@ -2,18 +2,32 @@ namespace generation {
 public class GenerationDriver {
 	
 	// 'Complementary' Generators
-	public class Complementary {}
+	public class Complementary {
+		public Complementary() {}
+	}
 	// 'Split Complementary' Generators
-	public class SplitComplementary {}
+	public class SplitComplementary {
+		public SplitComplementary() {}
+	}
 	// 'Triadic' Generators
-	public class Triadic {}
+	public class Triadic {
+		public Triadic() {}
+	}
 	// 'Tetradic' Generators
-	public class Tetradic {}
+	public class Tetradic {
+		public Tetradic() {}
+	}
 	// 'Square' Generators
-	public class Square {}
+	public class Square {
+		public Square() {}
+	}
 	// 'Analogous' Generators
-	public class Analogous {}
+	public class Analogous {
+		public Analogous() {}
+	}
 	// 'Monochromatic' Generators
-	public class Monochromatic {}
+	public class Monochromatic {
+		public Monochromatic() {}
+	}
 }
 }
