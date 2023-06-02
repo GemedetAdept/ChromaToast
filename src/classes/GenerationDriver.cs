951da2c18cb402e9ad58a18928e8b2c149704ae7
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-02 02:27:19 -0400

    Initalize classes for color harmony types.

--- src/classes/GenerationDriver.cs
+++ src/classes/GenerationDriver.cs
@@ -2,11 +2,18 @@ namespace generation {
 public class GenerationDriver {
 	
 	// 'Complementary' Generators
+	public class Complementary {}
 	// 'Split Complementary' Generators
+	public class SplitComplementary {}
 	// 'Triadic' Generators
+	public class Triadic {}
 	// 'Tetradic' Generators
+	public class Tetradic {}
 	// 'Square' Generators
+	public class Square {}
 	// 'Analogous' Generators
+	public class Analogous {}
 	// 'Monochromatic' Generators
+	public class Monochromatic {}
 }
 }
