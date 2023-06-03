e5549f6417707a879bb23a3fe6b667f79f696dfd
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-03 02:06:45 -0400

    Add Load methods for each Color type.

--- src/Drivers/DisplayDriver.cs
+++ src/Drivers/DisplayDriver.cs
@@ -14,7 +14,26 @@ public class SinglePalette : Display {
 	public Color.HSV[] HSVArray {get;set;}
 	public Color.RGB[] RGBArray {get;set;}
 
-	
+	public static int LoadCMYK(Color.CMYK[] inputCMYK) {
+		CMYKArray = inputCMYK;
+		return 1;
+	}
+	public static int LoadHEX(Color.HEX[] inputHEX) {
+		HEXArray = inputHEX;
+		return 1;
+	}
+	public static int LoadHSL(Color.HSL[] inputHSL) {
+		HSLArray = inputHSL;
+		return 1;
+	}
+	public static int LoadHSV(Color.HSV[] inputHSV) {
+		HSVArray = inputHSV;
+		return 1;
+	}
+	public static int LoadRGB(Color.RGB[] inputRGB) {
+		RGBArray = inputRGB;
+		return 1;
+	}
 
 	public static int PrintOutCMYK() {return 1;}
 }
