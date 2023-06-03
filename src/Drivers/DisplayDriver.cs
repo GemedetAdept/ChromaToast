7378ba3bd645c689f9497be9ecb820f667f6ffab
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-03 02:03:29 -0400

    Create SinglePalette polymorph of Display.

--- src/Drivers/DisplayDriver.cs
+++ src/Drivers/DisplayDriver.cs
@@ -1,5 +1,21 @@
+using colorvalues;
 namespace display {
 public class Display {
 	
+	public Display() {
+
+	}
+}
+
+public class SinglePalette : Display {
+	public Color.CMYK[] CMYKArray {get;set;}
+	public Color.HEX[] HEXArray {get;set;}
+	public Color.HSL[] HSLArray {get;set;}
+	public Color.HSV[] HSVArray {get;set;}
+	public Color.RGB[] RGBArray {get;set;}
+
+	
+
+	public static int PrintOutCMYK() {return 1;}
 }
 }
