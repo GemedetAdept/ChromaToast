0df41ff6837e1e36827709ce329495c01c40aad4
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-05 15:12:26 -0400

    Add Option class and DisplayMenu method to MenuGamma.

--- src/Menus/MenuGamma.cs
+++ src/Menus/MenuGamma.cs
@@ -1,5 +1,27 @@
 namespace menudriver {
 public class MenuGamma {
-	
+
+	public class Option {
+		public string Label {get;set;};
+		public Action Procedure {get;set;}
+
+		public Option(string label, Action procedure) {
+			Label = label;
+			Procedure = procedure;
+		}
+	}
+
+	public int DisplayMenu(Option[] options, Option activeOption) {
+
+		Console.Clear();
+
+		for (int i=0; i < options.Length; i++) {
+
+			if (options[i] == activeOption) {Console.WriteLine("> ");}
+			else {Console.WriteLine(" ");}
+
+			Console.WriteLine(options[i].Label);
+		}
+	}
 }
 }
