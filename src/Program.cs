8411a2f8ac4670017949a4a2835783c398f61c36
Vivian Zitek <vivian.zitek@gmail.com>
2023-06-05 23:17:31 -0400

    Remove old SaveToFile code in Program.

--- src/Program.cs
+++ src/Program.cs
@@ -26,40 +26,6 @@ MenuGamma gammaTest = new MenuGamma(testOptions);
 gammaTest.DisplayMenu();
 gammaTest.ExecuteProcedure(0);
 
-// foreach(Option option in testOptions) {
-// 	option.Procedure();
-// }
-
-// void savePalette(string fileName, string path = "") {
-
-// 	string[] headers = new string[]{
-// 		"CMYK",
-// 		"HEX",
-// 		"HSL",
-// 		"HSV",
-// 		"RGB",
-// 		"\n"
-// 	};
-
-// 	var outputFile = new SaveToFile.CSV(fileName, path);
-// 	if (outputFile.Initalize(outputFile.FileName)) {
-
-// 		outputFile.SetHeaders(outputFile.FileName, headers);
-
-// 		for (int i = 0; i < paletteHSV.Count; i++) {
-// 			string[] colors = new string[6];
-// 			colors[0] = "\"" + $"({paletteCMYK[i].Cyan}, {paletteCMYK[i].Magenta}, {paletteCMYK[i].Yellow}, {paletteCMYK[i].Key})" + "\"";
-// 			colors[1] = paletteHEX[i].Value;
-// 			colors[2] = "\"" + $"({paletteHSL[i].Hue}, {paletteHSL[i].Saturation}, {paletteHSL[i].Lightness})" + "\"";
-// 			colors[3] = "\"" + $"({paletteHSV[i].Hue}, {paletteHSV[i].Saturation}, {paletteHSV[i].Value})" + "\"";
-// 			colors[4] = "\"" + $"({paletteRGB[i].Red}, {paletteRGB[i].Green}, {paletteRGB[i].Blue})" + "\"";
-// 			colors[5] = "\n";
-
-// 			outputFile.WriteData(outputFile.FileName, colors);
-// 		}
-// 	}
-// }
-
 void DebugBox() {
 
 	TestsDriver.Initialize();
